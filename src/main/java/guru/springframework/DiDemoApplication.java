c642517ae7f24877e198fa75c5b0b1d7d52559d8
suayb <suaybsimsek58@gmail.com>
2019-05-10 10:54:59 +0300

    dependecy injection örneği yapıldı

---
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -0,0 +1,21 @@
+package guru.springframework;
+
+import guru.springframework.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx=SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController myController=(MyController) ctx.getBean("myController");
+        myController.hello();
+
+
+    }
+
+}
