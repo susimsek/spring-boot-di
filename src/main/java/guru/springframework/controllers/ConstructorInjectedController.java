233d0d2f296ba10841b9132c8f51c5f3a1dc8bd1
suayb <suaybsimsek58@gmail.com>
2019-05-11 10:55:26 +0300

    Qualifier anatasyonu örneği yapıldı

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -8,7 +9,7 @@ public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
